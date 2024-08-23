9703d78f3316ca7432baa9764e34a60447269847
César Esaú Flores Martínez <esauflores2022@gmail.com>
2024-08-24 00:06:10 +0700

    Updated daphne ASGI support

--- asgi.py
+++ asgi.py
@@ -1,7 +1,9 @@
 # asgi.py
-
+import socketio
 from asgiref.wsgi import WsgiToAsgi
 
-from app import app
+from app import app, socketio as app_socketio
 
-asgi_app = WsgiToAsgi(app)
+asgi = WsgiToAsgi(app)
+
+asgi_app = socketio.ASGIApp(app_socketio, asgi)
